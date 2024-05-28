8206d3f76994d310f8cde58df7f9f594af7848b0
nickop <nicolas.poy@outlook.com>
2024-05-28 00:09:42 -0300

    Added bullets

--- bullet.gd
+++ bullet.gd
@@ -1,11 +1,19 @@
 extends Node3D
 
-@export var speed = 70
+@export var speed = 30
+@onready var timer: Timer = $Timer
+# Preferred to use a Timer node. This is more for one shot timers 
+#@onready var timer: SceneTreeTimer = get_tree().create_timer(2)
 
 func _physics_process(delta):
+	var forward_direction: Vector3 = global_transform.basis.z.normalized()
 	"""
 		Basically doing distance = speed * time
 		e.g. translation = Vector3(0, 0, -1) * 70 * 0.5
 		With speed (70) being the magnitude of the velocity vector(70/1 - vector quantity)
 	"""
-	global_translate(Vector3.FORWARD * speed * delta)
+	# Using translate() is the same since the origin is the parent anyway
+	global_translate(forward_direction * speed * delta)
+	
+func _on_timer_timeout():
+	queue_free()
--- gun.gd
+++ gun.gd
@@ -3,18 +3,23 @@ extends Node3D
 @export var bullet: PackedScene
 @export var muzzle_speed = 30
 @export var time_between_shots = 100 # In milliseconds
+@onready var timer: Timer = $Timer
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var can_shoot: bool = true
 
 func _process(delta):
 	shoot()
 	
 func shoot():
-	var bullet = bullet.instantiate()
-	bullet.global_transform = $Muzzle.global_transform
-	#bullet.speed = muzzle_speed
-	# TODO: Refactor using the corresponding patter, why would we get the root like this?
-	var root_scene = get_tree().get_root().get_children()[0]
-	root_scene.add_child(bullet)
+	if can_shoot == true:
+		var bullet = bullet.instantiate()
+		bullet.global_transform = $Muzzle.global_transform # TODO: Check if using global is really needed in this scenario
+		bullet.speed = muzzle_speed
+		# TODO: Refactor using the corresponding pattern, why would we get the root like this?
+		var root_scene = get_tree().get_root().get_children()[0]
+		root_scene.add_child(bullet)
+		can_shoot = false
+		timer.start()
+
+func _on_timer_timeout():
+	can_shoot = true
