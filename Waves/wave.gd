813c71be0f7020b3da4fdedfe3398c05355c0ddc
nickop <nicolas.poy@outlook.com>
2024-06-19 01:59:12 -0300

    Added enemy waves -WIP (broken)-

---
+++ Waves/wave.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Wave
+
+"""
+	TODO: This nodes should be resources, not regular nodes
+"""
+@export var enemies_qty: int = 3
+@export var spawn_time: int = 2
+var spawned_enemies: int = 0
--- spawner.gd
+++ spawner.gd
@@ -1,13 +1,25 @@
 extends Node
 
 @export var enemy: PackedScene
-@export var enemies_qty = 10
 @onready var timer: Timer = $Timer
+#@export var enemies_qty: int = 3
+#@export var spawn_time: int = 2
+#var spawned_enemies: int = 0
+
+@export var waves: Array[Wave] = []
+var current_wave: Wave
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	waves = $Waves.get_childrens()
+	#timer.wait_time = spawn_time
+	timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_timer_timeout():
+	#if (spawned_enemies == enemies_qty):
+		#spawned_enemies = 0
+
+	#if (enemies_qty > spawned_enemies):
+		var newEnemy = enemy.instantiate()
+		get_parent().add_child(newEnemy)
+		#spawned_enemies += 1
