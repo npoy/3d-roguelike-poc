1c024e1be78a372407e1368b2e1ede719ad6aa69
nickop <nicolas.poy@outlook.com>
2024-09-09 09:03:08 -0300

    Level generator: Added random coords, rng seed and random height wip

--- Level/LevelGenerator/LevelGenerator.gd
+++ Level/LevelGenerator/LevelGenerator.gd
@@ -5,30 +5,65 @@ extends Node3D
 @export var ObstacleScene: PackedScene
 
 @export_range(1, 21) var map_width: int = 11:
-	get:
-		return map_width
+	get: return map_width
 	set(value):
 		map_width = value
 		generate_map()
 
 @export_range(1, 15) var map_depth: int = 11:
-	get:
-		return map_depth
+	get: return map_depth
 	set(value):
 		map_depth = value
 		generate_map()
 
-@export_range(0, 1, 0.05) var obstacle_ratio: float = 0.2:
-	get:
-		return obstacle_ratio
+@export_range(0, 1, 0.05) var obstacle_density: float = 0.2:
+	get: return obstacle_density
 	set(value):
-		obstacle_ratio = value
+		obstacle_density = value
 		generate_map()
+
+@export_range(1, 5) var obstacle_max_height: float = 5:
+	get: return obstacle_max_height
+	set(value):
+		obstacle_max_height = max(value, obstacle_min_height)
+		generate_map()
+
+@export_range(1, 5) var obstacle_min_height: float = 1:
+	get: return obstacle_min_height
+	set(value):
+		obstacle_min_height = min(value, obstacle_max_height)
+		generate_map()
+
+@export var rng_seed: int:
+	get: return rng_seed
+	set(value):
+		rng_seed = value
+		generate_map()
+
+var map_coords: Array = []
+
+class Coord:
+	var x: int
+	var z: int
+	
+	func _init(x: int, z: int):
+		self.x = x
+		self.z = z
+
+	func _to_string():
+		# (x, z)
+		return "{" + str(x) + ", " + str(z) + "}"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	generate_map() # TODO: Create make_odd method? check later
-	
+
+func fill_map_coords() -> void:
+	map_coords = []
+	for x in range(map_width):
+		for z in range(map_depth):
+			map_coords.append(Coord.new(x, z))
+
 func generate_map() -> void:
 	clear_map()
 	add_map()
@@ -46,12 +81,18 @@ func add_map():
 	add_child(ground)
 
 func add_obstacles():
-	for x in range(map_width):
-		for z in range(map_depth):
-			if(randf() < obstacle_ratio):
-				var obstacle_position: Vector3 = Vector3(x, 1, z)
-				obstacle_position -= Vector3(map_width/2, 0, map_depth/2)
-				var obstacle: CSGBox3D = ObstacleScene.instantiate()
-				obstacle.global_transform.origin = obstacle_position
-				add_child(obstacle)
-				
+	fill_map_coords()
+	seed(rng_seed)
+	map_coords.shuffle()
+	var obstacle_qty: int = map_coords.size() * obstacle_density
+	if (obstacle_qty > 0):
+		for coord in map_coords.slice(0, obstacle_qty):
+			add_obstacle_at(coord)
+	
+func add_obstacle_at(coord: Coord):
+	var obstacle_position: Vector3 = Vector3(coord.x, 1, coord.z)
+	obstacle_position -= Vector3(map_width/2, 0, map_depth/2)
+	var obstacle: CSGBox3D = ObstacleScene.instantiate()
+	obstacle.global_transform.origin = obstacle_position
+	add_child(obstacle)
+
